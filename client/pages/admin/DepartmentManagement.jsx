d3179c6ee4640e433178e8b6f857122f0e008037
ajojeda <ajojeda@me.com>
2025-04-18 16:06:56 -0700

    Add client/pages/admin/DepartmentManagement.jsx 1745017616329

---
+++ client/pages/admin/DepartmentManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function DepartmentManagement() {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-bold mb-4">Department Management</h2>
+      <p>This page will allow managing departments by site.</p>
+      {/* TODO: Implement department management UI */}
+    </div>
+  );
+}
