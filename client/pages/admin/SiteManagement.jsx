488155e30efaf6a0b768554dcf67fc2ccf90d777
ajojeda <ajojeda@me.com>
2025-04-18 16:06:55 -0700

    Add client/pages/admin/SiteManagement.jsx 1745017615425

---
+++ client/pages/admin/SiteManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SiteManagement() {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-bold mb-4">Site Management</h2>
+      <p>This page will allow managing site metadata.</p>
+      {/* TODO: Implement site management UI */}
+    </div>
+  );
+}
