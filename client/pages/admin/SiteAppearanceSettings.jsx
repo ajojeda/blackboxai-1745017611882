17fc46ad835473c284a32ab9d41b8f1293105ebc
ajojeda <ajojeda@me.com>
2025-04-18 16:06:54 -0700

    Add client/pages/admin/SiteAppearanceSettings.jsx 1745017614500

---
+++ client/pages/admin/SiteAppearanceSettings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SiteAppearanceSettings() {
+  return (
+    <div className="p-6">
+      <h2 className="text-2xl font-bold mb-4">Site Appearance Settings</h2>
+      <p>This page will allow managing logo and color branding per site.</p>
+      {/* TODO: Implement site appearance settings UI */}
+    </div>
+  );
+}
