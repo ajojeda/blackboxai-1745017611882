9a7ad59a6ce92c54af2264119665b690f623210a
ajojeda <ajojeda@me.com>
2025-04-18 16:07:17 -0700

    Add server/middleware/authenticateJWT.js 1745017637218

---
+++ server/middleware/authenticateJWT.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
+function authenticateJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Authorization header missing or malformed' });
+  }
+
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, config.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(401).json({ message: 'Invalid or expired token' });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = authenticateJWT;
